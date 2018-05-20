1f6b16562da7048161ebe8bb5b434af7a033a53c
el-davo <david.ahern26@gmail.com>
2018-05-20 22:12:56 +0100

    Added customer

---
+++ src/app/customer/customer-options/customer-options.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {CustomerService} from '../customer.service';
+import {Customer} from '../customer.model';
+
+@Component({
+  selector: 'app-customer-options',
+  templateUrl: './customer-options.component.html',
+  styleUrls: ['./customer-options.component.css']
+})
+export class CustomerOptionsComponent {
+
+  @Input() customer: Customer;
+  @Output() onDelete = new EventEmitter();
+
+  constructor(private service: CustomerService) {
+  }
+
+  deleteCustomer() {
+    this.service.deleteCustomer(this.customer).subscribe(() => this.onDelete.emit());
+  }
+
+}
