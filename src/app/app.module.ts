71aa02e8b59b76b2f4b7a9205117c328736126d9
el-davo <david.ahern26@gmail.com>
2018-05-20 22:12:50 +0100

    Added customer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
+import {AppComponent} from './app.component';
+import {CustomerModule} from './customer/customer.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomerComponent
-  ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    CustomerModule
+  ],
+  declarations: [
+    AppComponent
   ],
   providers: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
