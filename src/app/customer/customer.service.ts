b5bedf6210b8aa90a75a410e04aa51e8b01760e3
Joanna Wojcik <joanna.wojcik@mycit.ie>
2018-05-20 22:12:56 +0100

    Added customer

---
+++ src/app/customer/customer.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Customer} from './customer.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCustomers() {
+    return this.http.get<Customer[]>('https://customer-ms-java.herokuapp.com/customers/list');
+  }
+
+  deleteCustomer(customer: Customer) {
+    return this.http.delete(`https://customer-ms-java.herokuapp.com/customers/delete/${customer.id}`);
+  }
+}
