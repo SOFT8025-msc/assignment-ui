38a9b178e055b8c03b6686b963b7cd6386dd5789
el-davo <david.ahern26@gmail.com>
2018-05-21 20:16:49 +0100

    Added shipping address

--- src/app/customer/customer.model.ts
+++ src/app/customer/customer.model.ts
@@ -3,4 +3,15 @@ export interface Customer {
   firstName: string;
   lastName: string;
   title: string;
+  shippingAddress?: ShippingAddress
+}
+
+export interface ShippingAddress {
+  id: string;
+  line1: string;
+  line2: string;
+  postalCode: string;
+  city: string;
+  county: string;
+  country: string;
 }
